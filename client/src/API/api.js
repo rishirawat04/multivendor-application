b9e86e1baf0fc384fea841e83aaee8af2d064a78
Rishi Rawat <121020112+rishirawat04@users.noreply.github.com>
2024-11-03 14:22:18 +0530

    Update api.js

--- client/src/API/api.js
+++ client/src/API/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 // Create a global Axios instance
 const api = axios.create({
-  baseURL: 'https://ecom.anotechsolutions.com/api/v1'      
+  baseURL: 'https://multivendor-application.onrender.com/api/v1'      
 });
 
 export default api;
- // 'https://multivendor-application.onrender.com/api/v1'
+
